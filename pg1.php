ee710ef05d2102d45011ac5ef87850f97bdaf84f
beatrizcristiny <beatrizcrispires@gmail.com>
2026-05-31 14:12:56 -0300

    tentando deixar responsivo plmns um pouco

--- pg1.php
+++ pg1.php
@@ -13,6 +13,7 @@ $dia_sem = $dias[date("w")];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
     <title>Document</title>
 </head>
 <body>
@@ -22,7 +23,7 @@ $dia_sem = $dias[date("w")];
     </header>
 
 <main>
-    <p>
+    <p class = "texto">
         O que é geração dinâmica de páginas?
         Geração dinâmica significa que o PHP cria o conteúdo da página no momento em que alguém a acessa. Diferente do HTML puro, onde o conteúdo é fixo, o PHP consegue calcular e gerar informações em tempo real.
     </p>
@@ -44,18 +45,18 @@ $dia_sem = $dias[date("w")];
     </div>
     
     <h2>Tecnologias usadas nesta aplicação</h2>
-    <div class = "lingugens" >
-        <div class = "ling" >HTML5</div>
-        <div class = "ling">CSS3</div>
-        <div class = "ling">JavaScript</div>
-        <div class = "ling">PHP 8+</div>
+    <div class = "linguagens" >
+         <li>HTML5</li>
+        <li>CSS3></li>
+        <li>JavaScript</li>
+        <li>PHP 8+</li>
     </div>
     
     <h2>Por que isso é importante?</h2>
     <p>Se esta fosse uma página HTML pura, a data e hora estariam escritas no código e nunca mudariam. Com o PHP, essas informações são calculadas no servidor toda vez que alguém abre a página. Recarregue a página e veja a hora mudar!</p>
 </main>
 
-<footer>← Voltar para o início</footer>
+<footer><a href="index.php">← Voltar para o início</a></footer>
 </body>
 </html>
 
