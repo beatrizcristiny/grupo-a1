ec12cf53c3d62f09bf35f877a0288490ce955063
beatrizcristiny <beatrizcrispires@gmail.com>
2026-05-31 12:27:11 -0300

    tentando deixar responsivo plmns um pouco

--- pg1.php
+++ pg1.php
@@ -1,24 +1,63 @@
-Grupo A1 — Geração Dinâmica de Páginas
-Programação Web II — Exemplo 1
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+$data = date("d/m/Y");
+$hora = date("H:i:s");
 
-O que é geração dinâmica de páginas?
-Geração dinâmica significa que o PHP cria o conteúdo da página no momento em que alguém a acessa. Diferente do HTML puro, onde o conteúdo é fixo, o PHP consegue calcular e gerar informações em tempo real.
+$dias = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+$dia_sem = $dias[date("w")];
+?>
 
-Informações geradas pelo PHP
-Data de hoje:
-<?= $data ?>
-Hora atual:
-<?= $hora ?>
-Dia da semana:
-<?= $dia_semana ?>
 
-Tecnologias usadas nesta aplicação
-HTML5
-CSS3
-JavaScript
-PHP 8+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Grupo A1 — Geração Dinâmica de Páginas</h1>
+        <h2>Programação Web II — Exemplo 1</h2>
+    </header>
 
-Por que isso é importante?
-Se esta fosse uma página HTML pura, a data e hora estariam escritas no código e nunca mudariam. Com o PHP, essas informações são calculadas no servidor toda vez que alguém abre a página. Recarregue a página e veja a hora mudar!
+<main>
+    <p>
+        O que é geração dinâmica de páginas?
+        Geração dinâmica significa que o PHP cria o conteúdo da página no momento em que alguém a acessa. Diferente do HTML puro, onde o conteúdo é fixo, o PHP consegue calcular e gerar informações em tempo real.
+    </p>
+    
+    <h2>Informações geradas pelo PHP</h2>
+    <div class="informacoes">
+        <p>
+            Data de hoje:
+            <?= $data ?>
+        </p>
+        <p>
+            Hora atual:
+            <?= $hora ?>
+        </p>
+        <p>
+            Dia da semana:
+            <?= $dia_sem?>
+        </p>
+    </div>
+    
+    <h2>Tecnologias usadas nesta aplicação</h2>
+    <div class = "lingugens" >
+        <div class = "ling" >HTML5</div>
+        <div class = "ling">CSS3</div>
+        <div class = "ling">JavaScript</div>
+        <div class = "ling">PHP 8+</div>
+    </div>
+    
+    <h2>Por que isso é importante?</h2>
+    <p>Se esta fosse uma página HTML pura, a data e hora estariam escritas no código e nunca mudariam. Com o PHP, essas informações são calculadas no servidor toda vez que alguém abre a página. Recarregue a página e veja a hora mudar!</p>
+</main>
 
-← Voltar para o início
+<footer>← Voltar para o início</footer>
+</body>
+</html>
+
+
+
